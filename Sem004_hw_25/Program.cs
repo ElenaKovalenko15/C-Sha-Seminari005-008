b84b06d86fedefea317a383ec0a235c1c79163d4
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-13 12:05:01 +0200

    принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

---
+++ Sem004_hw_25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+
+  int numberA = GetNumberFromUser("Введите число A: ", "Ошибка ввода!");
+  int numberB = GetNumberFromUser("Введите число B: ", "Ошибка ввода!");
+
+  int exponent = Exponent(numberA, numberB);
+  Console.WriteLine($"Ответ: {exponent}");
+
+  int Exponent(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
