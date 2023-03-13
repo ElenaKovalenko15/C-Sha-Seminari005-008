02497ce4be2289cc58000566701412b23b88c7a9
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-13 12:14:45 +0200

    принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Sem004_hw_27/Program.cs
@@ -0,0 +1,35 @@
+﻿// принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+int numberN = GetNumberFromUser("Введите число: ", "Ошибка ввода!");
+int sumNumber = SumNumber(numberN);
+Console.WriteLine($"Сумма цифр в числе {sumNumber}");
+
+int SumNumber(int numberN)
+{
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
