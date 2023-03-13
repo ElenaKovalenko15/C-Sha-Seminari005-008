e2ab5b1594cf7e40915261ab5508bd3b1ac8dbc9
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-13 12:26:05 +0200

    задаёт массив из 8 элементов и выводит их на экран

---
+++ Sem004_hw_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+int arraySize = 8;
+int arrayMin = GetNumberFromUser("Введите минимальный элемент массива: ", "Ошибка ввода!");
+int arrayMax = GetNumberFromUser("Введите максимальный элемент массива: ", "Ошибка ввода!");
+int[] array = GetArray(arraySize, arrayMin, arrayMax);;
+
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
