00077912a26910b8deb0d4d1bb2c6a14fa4da19e
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-17 22:23:16 +0200

    первые N чисел Фибоначчи

---
+++ Sem006_task44/Program.cs
@@ -0,0 +1,35 @@
+﻿//не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два число Ф 0 и 1. 
+// если N = 5 -> 01123
+// если N = 3 -> 011
+// N = 7 -> 0112358
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое положительное число N: ", "Ошибка ввода!");
+int [] arrayFibon = GetFibonNumbers(num);
+Console.WriteLine($"{num} -> {String.Join(" ", arrayFibon)}");
+
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] GetFibonNumbers(int a)
+{
+    if(a==1)
+    return new int[1];
+    int[] result = new int[a];
+    result[1] = 1;
+    for (int i=2; i < a; i++)
+    {
+        result[i] = result[i -1] + result[i-2];
+    }
+    return result;
+}
