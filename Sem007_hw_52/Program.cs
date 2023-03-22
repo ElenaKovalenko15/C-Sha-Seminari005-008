23fdbf1ebb56e6640f3cc6227dc296302d8ae14c
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-22 09:26:26 +0200

    Найдите среднее арифметическое элементов в каждом столбце

---
+++ Sem007_hw_52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+//в каждом столбце. 
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+AverageArrColumn(array, rows, columns);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageArrColumn(int[,] array, int m, int n)
+{
+    double avsum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            avsum = (avsum + array[i, j]);
+        }
+        avsum = avsum / m;
+        Console.WriteLine($"среднее арифметическое элементов в {j} столбце -> {avsum}");
+        avsum = 0;
+    }
+}
