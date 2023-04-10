9959de1716b50b1063b65811d9646cf5bdc50404
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-04-10 11:42:35 +0300

    Напишите программу, которая выведет все натуральные числа в промежутке от N до 1

---
+++ Sem009_hw_64/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+int N = GetNumberFromUser("Введите натуральное положительное число: ", "Ошибка ввода!");
+int countM = 0;
+NaturalNumber(N, countM);
+
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
